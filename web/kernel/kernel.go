44804f81f00760f9b86683d7d6be2470b8d4e338
Shaumchen <shaumchen@outlook.com>
2022-05-30 21:29:20 +0800

    gin框架增加允许跨域

--- web/kernel/kernel.go
+++ web/kernel/kernel.go
@@ -3,6 +3,7 @@ package kernel
 import (
 	"gonitor/web/context"
 	"gonitor/web/exception"
+	"gonitor/web/middleware"
 )
 
 var Middleware []context.HandlerFunc
@@ -10,6 +11,7 @@ var Middleware []context.HandlerFunc
 func Load() {
 	Middleware = []context.HandlerFunc{
 		exception.Exception,
-		//session.Session,	//没有运行session 就不用打开
+		middleware.Cors,
+		//session.Session, //没有运行session 就不用打开
 	}
 }
--- web/middleware/middleware.go
+++ web/middleware/middleware.go
@@ -1,36 +1,35 @@
 package middleware
 
 import (
-	"errors"
-	"github.com/jinzhu/gorm"
-	"strconv"
-	"yogo/context"
-	"yogo/model"
-	"yogo/yogo"
+	"gonitor/web/context"
+	"net/http"
 )
 
-func M1(c *context.Context) {
-	//fmt.Println("1")
-}
-func M2(c *context.Context) {
-	//fmt.Println("2")
-}
-func M3(c *context.Context) {
-	//fmt.Println("3")
+func Cors(context *context.Context) {
+	method := context.Request.Method
+	context.Header("Access-Control-Allow-Origin", "*")
+	context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+	context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	context.Header("Access-Control-Allow-Credentials", "true")
+	if method == "OPTIONS" {
+		context.AbortWithStatus(http.StatusNoContent)
+	}
+	//context.Next()
 }
 
-func CheckToken(c *context.Context) {
-	token := c.Request.Header["Token"]
-	userToken := model.UserToken{}
-	dbResult := yogo.Db.Where("token = ?", token).Find(&userToken)
-	//没有找到  就返回错误就好了
-	if dbResult.Error != nil && errors.Is(dbResult.Error, gorm.ErrRecordNotFound) {
-		result := map[string]interface{}{
-			"code":    10001,
-			"message": "登录状态失效, 请重新登录",
-			"data":    nil,
-		}
-		c.AbortWithStatusJSON(200, result)
-	}
-	c.Request.Header.Add("user_id", strconv.FormatInt(userToken.UserID, 10))
-}
+//func CheckToken(c *context.Context) {
+//	token := c.Request.Header["Token"]
+//	userToken := model.UserToken{}
+//	dbResult := yogo.Db.Where("token = ?", token).Find(&userToken)
+//	//没有找到  就返回错误就好了
+//	if dbResult.Error != nil && errors.Is(dbResult.Error, gorm.ErrRecordNotFound) {
+//		result := map[string]interface{}{
+//			"code":    10001,
+//			"message": "登录状态失效, 请重新登录",
+//			"data":    nil,
+//		}
+//		c.AbortWithStatusJSON(200, result)
+//	}
+//	c.Request.Header.Add("user_id", strconv.FormatInt(userToken.UserID, 10))
+//}
